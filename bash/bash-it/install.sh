4336a1e29515657335e7495f5b07f799cbd53e98
winiex <winiex.nie@gmail.com>
2013-08-31 18:03:17 +0800

    Remove while true loop in the beginning of the install script

--- bash/bash-it/install.sh
+++ bash/bash-it/install.sh
@@ -1,32 +1,21 @@
 #!/usr/bin/env bash
 BASH_IT="$HOME/.bash_it"
 
-test -w $HOME/.bash_profile &&
-  cp $HOME/.bash_profile $HOME/.bash_profile.bak &&
-  echo "Your original .bash_profile has been backed up to .bash_profile.bak"
+read -p "Do you use Jekyll? (If you don't know what Jekyll is, answer 'n') [Y/N] " RESP
 
-cp $HOME/.bash_it/template/bash_profile.template.bash $HOME/.bash_profile
-
-echo "Copied the template .bash_profile into ~/.bash_profile, edit this file to customize bash-it"
-
-while true
-do
-  read -p "Do you use Jekyll? (If you don't know what Jekyll is, answer 'n') [Y/N] " RESP
-
-  case $RESP
-    in
-    [yY])
-      cp $HOME/.bash_it/template/jekyllconfig.template.bash $HOME/.jekyllconfig
-      echo "Copied the template .jekyllconfig into your home directory. Edit this file to customize bash-it for using the Jekyll plugins"
-      break
-      ;;
-    [nN])
-      break
-      ;;
-    *)
-      echo "Please enter Y or N"
-  esac
-done
+case $RESP
+  in
+  [yY])
+    cp $HOME/.bash_it/template/jekyllconfig.template.bash $HOME/.jekyllconfig
+    echo "Copied the template .jekyllconfig into your home directory. Edit this file to customize bash-it for using the Jekyll plugins"
+    break
+    ;;
+  [nN])
+    break
+    ;;
+  *)
+    echo "Please enter Y or N"
+esac
 
 function load_all() {
   file_type=$1
