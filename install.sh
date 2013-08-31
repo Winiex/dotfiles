5c3397044ec438382edcba477bf308ecdbc6105a
winiex <winiex.nie@gmail.com>
2013-08-31 14:28:35 +0800

    Add ASCII art in install.sh

--- install.sh
+++ install.sh
@@ -3,10 +3,18 @@
 #Run this script will install the dotfiles.
 
 #Clone the dotfiles from github.
+#Use this childish ASCII art to make it rock a little bit.
+echo "#######################################"
+echo "#  Now clone dotfiles into .dotfiles  #"
+echo "#######################################\n"
+
 git clone git@github.com:Winiex/dotfiles.git ~/.dotfiles
 cd ~/.dotfiles
 
 #Install vim configurations.
+echo "#######################################"
+echo "#  Now install vim configurations     #"
+echo "#######################################\n"
 
 git submodule init
 git submodule update
