d8b11da3157874730142a2b9cfcf6c245c2b6113
winiex <winiex.nie@gmail.com>
2013-09-22 12:15:10 +0800

    Add android tools custom path

---
+++ bash/bash-it/custom/android-path.bash
@@ -0,0 +1,10 @@
+export ANDROID_HOME=$HOME/software/android-bundle
+
+#Android sdk tools path
+PATH=$PATH:$ANDROID_HOME/sdk/tools:$ANDROID_HOME/sdk/platform-tools
+
+#Android ndk tools path
+PATH=$PATH:$ANDROID_HOME/ndk
+
+#Eclipse bin path
+PATH=$PATH:$ANDROID_HOME/eclipse
