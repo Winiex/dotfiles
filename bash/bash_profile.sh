a1bfdfdc9bba8c2ae026b0d20367cd10f7025aa6
winiex <winiex.nie@gmail.com>
2014-08-09 22:50:44 +0800

    Bash configs refactoring.

---
+++ bash/bash_profile.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+BASH_HOME=$HOME/.dotfiles/bash
+DISTRO=`uname -a`
+
+# Load configurations
+source $BASH_HOME/configs/bashit.sh
+source $BASH_HOME/configs/python.sh
+source $BASH_HOME/configs/rvm.sh
+source $BASH_HOME/configs/virtualenv.sh
