945e41a1cd3a169af623d037e2e09f64bfba8aed
winiex <winiex.nie@gmail.com>
2014-10-16 16:19:00 +0800

    Added git alias.

---
+++ bash/bash-it/custom/git-alias.bash
@@ -0,0 +1,7 @@
+alias git-log='git log --graph --all --format=format:'\''%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(bold yellow)%d%C(reset)%n'\'''\''          %C(white)%s%C(reset) %C(bold white)— %an%C(reset)'\'' --abbrev-commit'
+
+alias git-push-master="git push origin master"
+
+function git-ignore-io() {
+    curl https://www.gitignore.io/api/$@ ;
+}
