03f4d6c57d850fa5d69f71d8d6c79f16304d3571
winiex <winiex.nie@gmail.com>
2014-11-11 18:35:11 +0800

    Update octopress tools.

--- bash/bash-it/custom/octopress-alias.bash
+++ bash/bash-it/custom/octopress-alias.bash
@@ -1,4 +1,7 @@
-alias octopress-publish="(cblog && git-update-submodules-master && rake deploy)"
+alias octopress-publish="(cblog && git-update-submodules-master && rake generate && rake deploy)"
+alias octopress-update="(cblog && git-update-submodules-master)"
+alias octopress-push-source="(cblog && gco source && git add --all && git commit -m \"Update automatically.\" && git push origin source)"
+
 alias octopress-deploy="(cblog && rake deploy)"
 alias octopress-preview="(cblog && rake preview)"
 alias octopress-generate="(cblog && rake generate)"
